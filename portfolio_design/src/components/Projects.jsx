3347826f6f5f0ddfc7ce19fe221308c180d67584
kawsar firoz <kawsarfiroz11@gmail.com>
2026-04-26 08:44:41 +0600

    updated project desc

--- portfolio_design/src/components/Projects.jsx
+++ portfolio_design/src/components/Projects.jsx
@@ -13,17 +13,19 @@ const Projects = () => {
         "A hospital patient data analytics dashboard built as a custom alternative to Power BI. It provides  pagination, multi-column filtering, patient statistics, admission trends, disease-wise analysis, and interactive charts for real-time healthcare insights using React, Python Pandas, and Chart.js.",
       tech: ["React JS", "Python", "Pandas", "JSON", "Chart.js", "Matplotlib"],
       type: "Healthcare Analytics Dashboard",
-      github: "https://github.com",
+      github: "https://github.com/kawsar334/data-analytics-portfolio/tree/main/hospitalPatient",
+      excel:"https://docs.google.com/spreadsheets/d/17kfSLWRd2ePLaYhhphwv9WPgydQ9Gtz2XK5yskEWCxc/edit?usp=sharing",
       live: "https://hospitalpatientsdasboard.netlify.app/",
       icon: <FaTable className="text-blue" />
     },
     {
       title: "STUDENT  PERFORMANCE ANALYTICS  ",
-      description: "i cleaned student performance  missing values , analyze data student performance  using matplotlib,  for these information :Hours_Studied,Attendance,Parental_Involvement,Access_to_Resources,Extracurricular_Activities,Sleep_Hours,Previous_Scores,Motivation_Level,Internet_Access,Tutoring_Sessions,Family_Income,Teacher_Quality,School_Type,Peer_Influence,Physical_Activity,Learning_Disabilities,Parental_Education_Level,Distance_from_Home,Gender,Exam_Score",
+      description: "A data analysis project focused on student performance evaluation. The dataset was cleaned and preprocessed by handling missing values, followed by exploratory data analysis using Matplotlib to uncover key insights across academic, behavioral, and socio-economic factors influencing exam scores.",
       tech: [ "Python", "Pandas", "JSON", "Matplotlib"],
       type: "Student data analyst dashboard",
       github: "#",
       live: "#",
+      excel:"#",
       // icon: <FaDatabase className="text-emerald-400" />
       icon: <FaTable className="text-blue" />
     },
@@ -32,8 +34,9 @@ const Projects = () => {
       description: "i clean the missing values using pandas , modified column names , and added in to sql database  , and analyz data using matplotlib , and seaborn , ",
       tech: [ "Python", "Pandas", "JSON", "Matplotlib",'seaborn'],
       type: "Student data analyst dashboard",
-      github: "#",
+      github: "https://github.com/kawsar334/data-analytics-portfolio/blob/main/customer_shoping_behaviorsss/customer_shoping_behaviors.ipynb",
       live: "#",
+      excel:"#",
       // icon: <FaDatabase className="text-emerald-400" />
       icon: <FaTable className="text-blue" />
     },
@@ -71,11 +74,11 @@ const Projects = () => {
               <div className="p-8 bg-borderColor rounded-[calc(1.5rem-1px)] h-full flex flex-col justify-between parrentcard">
                 <div className=''>
                   <div className="flex  justify-between items-start mb-6">
-                    <a title="google sheet " href='#' target='_blank' className="p-4 bg-slate-900 rounded-2xl border border-slate-700 group-hover:scale-110 transition-transform">
+                    <a title="google sheet " href={project.excel} target='_blank' className="p-4 bg-slate-900 rounded-2xl border border-slate-700 group-hover:scale-110 transition-transform">
                       {project.icon}
                     </a>
                     <div className="flex gap-5 ">
-                      <a href={project.github} title="Source Code" className="text-slate-400 hover:text-white transition-colors">
+                      <a href={project.github} target='_blank' title="Source Code" className="text-slate-400 hover:text-white transition-colors">
                         <FaGithub size={22} />
                       </a>
                       <a href={project.live} target="_blank" rel="noreferrer" title="Live Preview" className="text-blue hover:text-blue-300 transition-colors flex items-center gap-1 font-semibold border-blue  border-1 border px-3  py-1  rounded-full ">
